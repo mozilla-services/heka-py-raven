3147a0f0f8016625146cc5d754a21a66ccb15943
Victor Ng <victor@crankycoder.com>
2012-04-30 14:42:09 -0400

    reworked code and tests to use the new MetlogClientHolder

--- metlog_raven/raven_plugin.py
+++ metlog_raven/raven_plugin.py
@@ -5,18 +5,21 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 # ***** END LICENSE BLOCK *****
 
-# Code to reconstruct the frame as a JSON serializable 
-from raven.utils import varmap
-from raven.utils.encoding import shorten
-from raven.utils.stacks import get_culprit
-from raven.utils.stacks import get_stack_info
-from raven.utils.stacks import iter_traceback_frames
+# Code to reconstruct the frame as a JSON serializable
+from raven import Client
 
 import sys
 
 from metlog.decorators.base import MetlogDecorator
-from metlog.decorators.base import CLIENT_WRAPPER
+from metlog.holder import get_client
 from metlog.client import SEVERITY
+
+
+class RavenClient(Client):
+    def send(self, **data):
+        """ Sends messages into metlog """
+        self._metlog_msg = self.encode(data)
+
 
 class capture_stack(MetlogDecorator):
     """
@@ -25,15 +28,9 @@ class capture_stack(MetlogDecorator):
     exception will be re-raised.  It is *still* the responsibility of
     callers to handle exceptions properly.
 
-    :param str_length: This is the maximum length of each traceback string.
-                  Default is 200 characters
-    :param list_length: The number of stack frames which will be captured
-                   by the logger. Default is 50 frames.
-    :param payload: The default message that will be sent with each stacktrace.
-           By default this string is empty
     :param severity: The default severity of the error.  Default is 3 as
-      defined by `metlog.client:SEVERITY.ERROR` 
-      <https://github.com/mozilla-services/metlog-py/blob/master/metlog/client.py>
+      defined by `metlog.client:SEVERITY.ERROR`
+      <https://github.com/mozilla-services/metlog-py/blob/master/metlog/client.py> # NOQA
 
     The logger name will automatically be set the the fully qualified
     name of the decorated function.
@@ -43,37 +40,26 @@ class capture_stack(MetlogDecorator):
     def metlog_call(self, *args, **kwargs):
         if self.kwargs is None:
             self.kwargs = {}
-        str_length = self.kwargs.pop('str_length', 200)
-        list_length = self.kwargs.pop('list_length', 50)
         severity = self.kwargs.pop('severity', SEVERITY.ERROR)
-        payload = self.kwargs.pop('payload', None)
 
         try:
             result = self._fn(*args, **kwargs)
             return result
-        except Exception, e:
-            exc_info = sys.exc_info()
-            exc_type, exc_value, exc_traceback = exc_info
+        except:
 
-            frames = varmap(lambda k, v: shorten(v,
-                string_length=str_length,
-                list_length=list_length),
-                get_stack_info(iter_traceback_frames(exc_traceback)))
+            rc = RavenClient()
+            rc.captureException(sys.exc_info())
+            payload = rc._metlog_msg
 
-            culprit = get_culprit(frames)
-
-            metlog_blob = {'culprit': culprit,
-                    'frames': frames}
-
-            CLIENT_WRAPPER.client.metlog('stacktrace',
+            get_client('metlog.sentry').metlog(type='sentry',
                     logger=self._fn_fq_name,
-                    severity=severity,
                     payload=payload,
-                    fields=metlog_blob)
+                    severity=severity)
 
             # re-raise the exception so that callers up the call stack
             # have a chance to do the right thing
             raise
+
 
 def config_plugin(config):
     """
@@ -89,44 +75,27 @@ def config_plugin(config):
     :param payload: The default message that will be sent with each stacktrace.
            By default this string is empty
     :param severity: The default severity of the error.  Default is 3 as
-      defined by `metlog.client:SEVERITY.ERROR` 
-      <https://github.com/mozilla-services/metlog-py/blob/master/metlog/client.py>
+      defined by `metlog.client:SEVERITY.ERROR`
+      <https://github.com/mozilla-services/metlog-py/blob/master/metlog/client.py> # NOQA
 
     """
 
-    default_str_length = config.pop('str_length', 200)
-    default_list_length = config.pop('list_length', 50)
     default_logger = config.pop('logger', None)
-    default_payload= config.pop('payload', None)
     default_severity = config.pop('severity', SEVERITY.ERROR)
+    rc = RavenClient()
 
-    def metlog_exceptor(self, logger=default_str_length,
-            payload=default_payload,
-            str_length=default_str_length,
-            list_length=default_list_length,
-            severity=default_severity,
-            exc_info=None):
+    def metlog_exceptor(self, logger=default_logger, severity=default_severity,
+                exc_info=None):
+
         if exc_info is None:
             exc_info = sys.exc_info()
 
-        exc_type, exc_value, exc_traceback = exc_info
+        rc.captureException(exc_info)
+        payload = rc._metlog_msg
 
-        frames = varmap(lambda k, v: shorten(v,
-            string_length=str_length,
-            list_length=list_length),
-            get_stack_info(iter_traceback_frames(exc_traceback)))
-
-        culprit = get_culprit(frames)
-
-        metlog_blob = {'culprit': culprit,
-                'frames': frames}
-
-        self.metlog(type='stacktrace',
+        self.metlog(type='sentry',
                 logger=logger,
                 payload=payload,
-                severity=severity,
-                fields=metlog_blob)
+                severity=severity)
 
     return metlog_exceptor
-
-
--- metlog_raven/tests/test_metlog.py
+++ metlog_raven/tests/test_metlog.py
@@ -5,20 +5,23 @@
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 # ***** END LICENSE BLOCK *****
 
-from metlog.client import MetlogClient
 from metlog.client import SEVERITY
-from metlog.decorators.base import CLIENT_WRAPPER
+from metlog.holder import CLIENT_HOLDER
+from metlog.holder import get_client
+from metlog_raven.raven_plugin import RavenClient
 from metlog_raven.raven_plugin import capture_stack
-from mock import Mock
 from nose.tools import eq_
 import json
 
+
 def exception_call2(a, b, c):
-    return a + b + c / (a-b)
+    return a + b + c / (a - b)
+
 
 @capture_stack
 def exception_call1(x, y):
     return exception_call2(y, x, 42)
+
 
 @capture_stack
 def clean_exception_call(x, y):
@@ -27,32 +30,34 @@ def clean_exception_call(x, y):
 
 class DecoratorTestBase(object):
     def setUp(self):
-        self.orig_client = CLIENT_WRAPPER.client
-        client_config = {
-            'sender_class': 'metlog.senders.DebugCaptureSender',
-            }
-        CLIENT_WRAPPER.activate(client_config)
+        self.orig_client = get_client('test', \
+                {'sender_class': 'metlog.senders.DebugCaptureSender'})
 
-    def tearDown(self):
-        CLIENT_WRAPPER.client = self.orig_client
+        # Clobber the sentry plugin
+        CLIENT_HOLDER._clients['metlog.sentry'] = self.orig_client
+
 
 class TestCannedDecorators(DecoratorTestBase):
-
     def test_capture_stack(self):
         msgs = []
         try:
-            result = exception_call1(5, 5)
-        except Exception, e:
-            msgs = [json.loads(m) for m in CLIENT_WRAPPER.client.sender.msgs]
+            exception_call1(5, 5)
+        except:
+            msgs = [json.loads(m) for m in self.orig_client.sender.msgs]
 
         # There should be 1 exception
         eq_(len(msgs), 1)
 
         # We should have a culprit of exception_call2
         event = msgs[0]
-        eq_(event['fields']['culprit'],
+
+        rc = RavenClient()
+        sentry_fields = rc.decode(event['payload'])
+
+        eq_(sentry_fields['culprit'],
             'test_metlog.exception_call2')
-        culprit_frame = [f for f in event['fields']['frames'] \
+        frames = sentry_fields['sentry.interfaces.Stacktrace']['frames']
+        culprit_frame = [f for f in frames \
                             if f['function'] == 'exception_call2'][0]
         # Check for the variables that cause the divide by zero
         eq_(culprit_frame['vars']['a'],
@@ -63,6 +68,7 @@ class TestCannedDecorators(DecoratorTestBase):
 
     def test_capture_stack_passing(self):
         eq_(25, clean_exception_call(5, 5))
+
 
 def test_plugins_config():
     cfg_txt = """
@@ -78,7 +84,7 @@ def test_plugins_config():
     client = client_from_text_config(cfg_txt, 'metlog')
 
     def exception_call2(a, b, c):
-        return a + b + c / (a-b)
+        return a + b + c / (a - b)
 
     def exception_call1(x, y):
         return exception_call2(y, x, 42)
@@ -86,13 +92,17 @@ def test_plugins_config():
     try:
         exception_call1(5, 5)
     except:
-        client.raven('some_logger_name', 'Caught an error')
+        client.raven('some_logger_name')
 
     eq_(1, len(client.sender.msgs))
 
     msg = json.loads(client.sender.msgs[0])
-    assert msg['fields']['culprit'] == 'test_metlog.exception_call2'
-    assert len(msg['fields']['frames']) == 3
-    assert msg['logger'] == 'some_logger_name'
-    assert msg['type'] == 'stacktrace'
-    assert msg['severity'] == SEVERITY.ERROR
+
+    rc = RavenClient()
+    sentry_fields = rc.decode(msg['payload'])
+    eq_(sentry_fields['culprit'], 'test_metlog.exception_call2')
+    eq_(len(sentry_fields['sentry.interfaces.Stacktrace']['frames']), 3)
+
+    eq_(msg['logger'], 'some_logger_name')
+    eq_(msg['type'], 'sentry')
+    eq_(msg['severity'], SEVERITY.ERROR)
